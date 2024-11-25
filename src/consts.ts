026c45cfb8d3aa7ea874222e16c652f9068f14e5
yoezequiel <ezequielcasiano15@gmail.com>
2024-11-25 16:03:39 -0300

    add cards founder - Agrega un diccionario en const con toda la info de los miembros - Usar ese diccionario en el about para poner la informacion de cada miembro en la card

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,46 @@
 
 export const SITE_TITLE = 'CROBF';
 export const SITE_DESCRIPTION = 'Empresa de desarrollo de software';
+
+export const TEAM_MEMBERS = {
+  Juan: {
+    fullName: "Beresiarte Juan Basiluk",
+    logo:"/founders/juan_beresiarte.jpeg",
+    email: "juanberesiarte@gmail.com",
+    gitHub:"https://github.com/beresiartejuan",
+    linkedin:"https://www.linkedin.com/in/beresiartejuan/",
+    link:"/founders/juan_beresiarte"
+  },
+  Ezequiel: {
+    fullName: "Casiano Ezequiel",
+    logo:"",
+    email: "ezequielcasiano15@gmail.com",
+    gitHub:"https://github.com/yoezequiel",
+    linkedin:"https://www.linkedin.com/in/yoezequiel/",
+    link:"/founders/ezequiel_casiano"
+  },
+  Lautaro: {
+    fullName: "Ferreira Lautaro",
+    logo:"",
+    email: "Lauferreyraff@gmail.com",
+    gitHub:"https://github.com/lautaroff",
+    linkedin:"",
+    link:"/founders/lautaro_ferreira"
+  },
+  Valentina: {
+    fullName: "Osorio Valentina",
+    logo:"",
+    email: "valentina.osorio2203@gmail.com",
+    gitHub:"https://github.com/Valen2203",
+    linkedin:"",
+    link:"/founders/valentina_osorio"
+  },
+  Magali: {
+    fullName: "Rodriguez Magali",
+    logo:"",
+    email: "magalidaianarodriguezm@gmail.com",
+    gitHub:"https://github.com/Magali22R",
+    linkedin:"",
+    link:"/founders/magali_rodriguez"
+  },
+};
