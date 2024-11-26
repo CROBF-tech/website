121d3f90b12c9d8be06f3737c2da4519a25ef8a9
juan beresiarte <juanberesiarte@gmail.com>
2024-11-25 21:44:50 -0300

    Add magali photo

--- src/consts.ts
+++ src/consts.ts
@@ -7,42 +7,42 @@ export const SITE_DESCRIPTION = 'Empresa de desarrollo de software';
 export const TEAM_MEMBERS = {
   Juan: {
     fullName: "Beresiarte Juan Basiluk",
-    logo:"/founders/juan_beresiarte.jpeg",
+    logo: "/founders/juan_beresiarte.jpeg",
     email: "juanberesiarte@gmail.com",
-    gitHub:"https://github.com/beresiartejuan",
-    linkedin:"https://www.linkedin.com/in/beresiartejuan/",
-    link:"/founders/juan_beresiarte"
+    gitHub: "https://github.com/beresiartejuan",
+    linkedin: "https://www.linkedin.com/in/beresiartejuan/",
+    link: "/founders/juan_beresiarte"
   },
   Ezequiel: {
     fullName: "Casiano Ezequiel",
-    logo:"",
+    logo: "/founders/carpincho.jpg",
     email: "ezequielcasiano15@gmail.com",
-    gitHub:"https://github.com/yoezequiel",
-    linkedin:"https://www.linkedin.com/in/yoezequiel/",
-    link:"/founders/ezequiel_casiano"
+    gitHub: "https://github.com/yoezequiel",
+    linkedin: "https://www.linkedin.com/in/yoezequiel/",
+    link: "/founders/ezequiel_casiano"
   },
   Lautaro: {
     fullName: "Ferreira Lautaro",
-    logo:"",
+    logo: "",
     email: "Lauferreyraff@gmail.com",
-    gitHub:"https://github.com/lautaroff",
-    linkedin:"",
-    link:"/founders/lautaro_ferreira"
+    gitHub: "https://github.com/lautaroff",
+    linkedin: "",
+    link: "/founders/lautaro_ferreira"
   },
   Valentina: {
     fullName: "Osorio Valentina",
-    logo:"",
+    logo: "",
     email: "valentina.osorio2203@gmail.com",
-    gitHub:"https://github.com/Valen2203",
-    linkedin:"",
-    link:"/founders/valentina_osorio"
+    gitHub: "https://github.com/Valen2203",
+    linkedin: "",
+    link: "/founders/valentina_osorio"
   },
   Magali: {
     fullName: "Rodriguez Magali",
-    logo:"",
+    logo: "/founders/magali_rodriguez.jpg",
     email: "magalidaianarodriguezm@gmail.com",
-    gitHub:"https://github.com/Magali22R",
-    linkedin:"",
-    link:"/founders/magali_rodriguez"
+    gitHub: "https://github.com/Magali22R",
+    linkedin: "",
+    link: "/founders/magali_rodriguez"
   },
 };
