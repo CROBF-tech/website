e79427c5f76e41099d9f711311e5906c1aae248d
juan beresiarte <juanberesiarte@gmail.com>
2024-11-26 22:44:39 -0300

    Add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,29 +4,35 @@ import mdx from "@astrojs/mdx";
 
 import sitemap from "@astrojs/sitemap";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://crobf.vercel.app",
-    integrations: [mdx(), sitemap()],
-    vite: {
-        css: {
-            preprocessorOptions: {
-                sass: {
-                    api: 'modern-compiler'
-                },
-                scss: {
-                    api: 'modern-compiler'
-                }
-            }
-        }
-    },
-    i18n: {
-        locales: ["es", "en"],
-        defaultLocale: "en",
-        routing: {
-            prefixDefaultLocale: true,
-            redirectToDefaultLocale: true
-        },
-    },
-    output: "server"
+  site: "https://crobf.vercel.app",
+  integrations: [mdx(), sitemap()],
+
+  vite: {
+      css: {
+          preprocessorOptions: {
+              sass: {
+                  api: 'modern-compiler'
+              },
+              scss: {
+                  api: 'modern-compiler'
+              }
+          }
+      }
+  },
+
+  i18n: {
+      locales: ["es", "en"],
+      defaultLocale: "en",
+      routing: {
+          prefixDefaultLocale: true,
+          redirectToDefaultLocale: true
+      },
+  },
+
+  output: "server",
+  adapter: vercel()
 });
