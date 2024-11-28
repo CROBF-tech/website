f49376cd1e78dec8bc484db83e49154d1af9bd4b
juan beresiarte <juanberesiarte@gmail.com>
2024-11-28 00:33:37 -0300

    ADD LANG

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
 import sitemap from "@astrojs/sitemap";
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
@@ -20,4 +21,8 @@ export default defineConfig({
             },
         },
     },
+    output: "server",
+    adapter: node({
+        mode: "standalone"
+    })
 });
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,42 @@
 import { defineMiddleware } from "astro:middleware";
+import { defaultLang, ui } from "./i18n/ui";
 
-export const onRequest = defineMiddleware((context, next) => {
-    const validLangs = ["es", "en"];
-    const lang = context.url.pathname.split('/')[1];
+const language_cookie_name = "prefered_language";
+const cookies_config = {
+    path: "/", // Configuración para que la cookie esté disponible en la raíz
+    httpOnly: false
+};
 
-    if (!validLangs.includes(lang)) {
-        return context.redirect('/en');
+export const onRequest = defineMiddleware(async (context, next) => {
+    const { cookies } = context;
+    const validLangs = Object.keys(ui);
+    const [url_lang, ...url_path] = context.url.pathname.split('/').slice(1);
+
+    // Obtén el idioma preferido desde la cookie
+    let prefered_language = cookies.get(language_cookie_name)?.value;
+
+    // Si no hay cookie configurada, establece una según la URL o el idioma por defecto
+    if (!prefered_language) {
+        prefered_language = validLangs.includes(url_lang) ? url_lang : defaultLang;
+        cookies.set(language_cookie_name, prefered_language, cookies_config);
+
+        if (!validLangs.includes(url_lang)) {
+            return context.redirect(`/${prefered_language}/${url_path.join('/')}`);
+        }
+        return next();
     }
 
-    next();
+    // Si el idioma en la cookie no es válido, corrige la cookie y redirige
+    if (!validLangs.includes(prefered_language)) {
+        cookies.set(language_cookie_name, defaultLang, cookies_config);
+        return context.redirect(`/${defaultLang}/${url_path.join('/')}`);
+    }
+
+    // Si el idioma en la URL no coincide con la cookie, redirige
+    if (url_lang !== prefered_language) {
+        return context.redirect(`/${prefered_language}/${url_path.join('/')}`);
+    }
+
+    // Procede con la solicitud
+    return next();
 });
