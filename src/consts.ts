bf9f20e4122be3d361d2e92f563ffb95700df174
yoezequiel <ezequielcasiano15@gmail.com>
2025-01-27 16:06:39 -0300

    add new members in about page

--- src/consts.ts
+++ src/consts.ts
@@ -14,7 +14,6 @@ export const TEAM_MEMBERS = {
     link: "/founders/juan_beresiarte",
     profession: "Desarrollador Full Stack",
     description: "Estudiante de la tecnicatura universitaria en programación en la UTN. Me apasiona la tecnología y el desarrollo de software. Me gusta aprender cosas nuevas y enfrentar desafíos. Soy una persona proactiva, responsable y comprometida con mi trabajo.",
-    index: 4
   },
   Ezequiel: {
     fullName: "Casiano Ezequiel",
@@ -25,39 +24,65 @@ export const TEAM_MEMBERS = {
     link: "/founders/ezequiel_casiano",
     profession: "Desarrollador Full Stack",
     description: "¡Hola! Soy Casiano Ezequiel, estudiante de desarrollo de software con enfoque en desarrollo web y Python. Siempre dispuesto a explorar y aprender más.",
-    index: 1
   },
   Lautaro: {
     fullName: "Ferreira Lautaro",
     logo: "/founders/lautaro_ferreira.jpg",
     email: "Lauferreyraff@gmail.com",
     gitHub: "https://github.com/lautaroff",
-    linkedin: "",
+    linkedin: "https://www.linkedin.com/in/lautaro-francisco-ferreyra-217743244",
     link: "/founders/lautaro_ferreira",
     profession: "Desarrollador Full Stack",
     description: "¡Hola! Soy Lautaro Ferreyra, estudiante de la Tecnicatura Superior en Desarrollo de Software, con un marcado interés en el desarrollo backend y la creación de soluciones tecnológicas robustas.",
-    index: 5
   },
-  Valentina: {
-    fullName: "Osorio Valentina",
-    logo: "/founders/valentina_osorio.jpeg",
-    email: "valentina.osorio2203@gmail.com",
-    gitHub: "https://github.com/Valen2203",
-    linkedin: "",
-    link: "/founders/valentina_osorio",
-    profession: "Desarrollador Full Stack",
-    description: "Soy una estudiante de programación de software con una marcada preferencia por el diseño de páginas web. ",
-    index: 3,
+  Sofia:{
+    fullName: "Rubio Sofia",
+    logo: "/founders/sofia_rubio.jpeg",
+    email: "sofianrubio8@gmail.com",
+    gitHub: "https://github.com/SofiaNRubio",
+    linkedin: "https://www.linkedin.com/in/sofi-rubio-54a2b4278/",
+    link: "/founders/sofia_rubio",
+    profession: "Desarrolladora Full Stack",
+    description: "Hola, soy Sofia Rubio, estudiante de Desarrollo de Software. Soy una persona comprometida y responsable, me gusta el desarrollo frontend y estoy a disposición para abordar desafíos en el backend.",
   },
-  Magali: {
-    fullName: "Rodriguez Magali",
-    logo: "/founders/magali_rodriguez.jpg",
-    email: "magalidaianarodriguezm@gmail.com",
-    gitHub: "https://github.com/Magali22R",
-    linkedin: "",
-    link: "/founders/magali_rodriguez",
-    profession: "Desarrollador Full Stack",
-    description: "Soy una estudiante de desarrollo de software con una gran ambición por mejorar y convertirme en una profesional algún día.",
-    index: 2
+  Julieta:{
+    fullName:"Sotelo Julieta",
+    logo: "/founders/julieta_sotelo.jpeg",
+    email: "soteloanzorena@gmail.com",
+    gitHub: "https://github.com/JuuSotelo",
+    linkedin: "https://www.linkedin.com/in/julieta-sotelo-54181a347",
+    link: "/founders/julieta_sotelo",
+    profession: "Diseño, Análisis, Fullstack",
+    description:"¡Hola! Soy Julieta Sotelo, estudiante de la Tecnicatura Superior en Desarrollo de software explorando el camino de la programación y diseño web."
   },
+  Santiago:{
+    fullName:"Suarez Santiago",
+    logo: "/founders/santiago_suarez.jpeg",
+    email: "santiago.suarez9898@gmail.com",
+    gitHub: "https://github.com/SantiagoSuarez9",
+    linkedin: "https://www.linkedin.com/in/santiago-suarez-96a61a341/",
+    link: "/founders/santiago_suarez",
+    profession: "Desarrollador Backend",
+    description:"Hola! Soy Santiago Suarez, estudiante de la Tecnicatura Superior en Desarrollo de Software. Me apasiona analizar y resolver problemas, disfrutando del desafío que implica descomponer situaciones complejas para encontrar soluciones prácticas y efectivas."
+  },
+  Paula:{
+    fullName: "Toledo Ana Paula",
+    logo: "/founders/ana_paula_toledo.jpeg",
+    email: "anapaulatoledo2003@gmail.com",
+    gitHub: "https://github.com/paulytoledoo",
+    linkedin: "https://www.linkedin.com/in/ana-paula-toledo-a27527261/",
+    link: "/founders/ana_paula_toledo",
+    profession: "Desarrollo Backend, Análisis, Documentación, Ciencia de Datos. ",
+    description:"¡Hola! Soy Ana Paula, estudiante de la Tecnicatura Superior en Desarrollo de Software, apasionada por encontrar soluciones creativas e innovadoras a problemas tecnológicos.Mi capacidad autodidacta me permite aprender nuevas habilidades de forma constante."
+  },
+  Silvina:{
+    fullName: "Olivares Silvina",
+    logo: "/founders/silvina_olivares.jpeg",
+    email: "silvinaolivares78@gmail.com",
+    gitHub: "https://github.com/silvimgl",
+    linkedin: "https:https://www.linkedin.com/in/silvinaolivares/",
+    link: "/founders/silvina_olivares",
+    profession: "Diseño UX UI",
+    description:"¡Hola! Soy Silvina y soy estudiante de Diseño UX/UI con conocimientos en conceptos básicosMi objetivo es colaborar en la creación de productos intuitivos y funcionales mientras desarrollo mis competencias en investigación, diseño y mejora de experiencias digitales."
+  }
 };
