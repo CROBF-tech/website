87dcd9217e8e00f7859cda7fcb7d94570e73413c
yoezequiel <ezequielcasiano15@gmail.com>
2026-02-17 15:26:35 -0300

    feat: add Ezequiel Casiano to team members and update profile details

--- src/consts.ts
+++ src/consts.ts
@@ -19,6 +19,18 @@ export const TEAM_MEMBERS = {
             'Estudiante de la tecnicatura universitaria en programación en la UTN. Me apasiona la tecnología y el desarrollo de software. Me gusta aprender cosas nuevas y enfrentar desafíos. Soy una persona proactiva, responsable y comprometida con mi trabajo.',
         index: 1,
     },
+    Ezequiel: {
+        fullName: 'Ezequiel Casiano',
+        logo: '/founders/ezequiel_casiano.png',
+        email: 'ezequielcasiano@gmail.com',
+        gitHub: 'https://github.com/yoezequiel',
+        linkedin: 'https://www.linkedin.com/in/yoezequiel',
+        link: '/founders/ezequiel_casiano',
+        profession: 'Desarrollador Full Product',
+        description:
+            'Líder: define visión y roadmap, prioriza por valor y coordina equipos cross‑funcionales para lanzar productos escalables y medibles.',
+        index: 2,
+    },
     Leandro: {
         fullname: 'Leandro Diaz',
         logo: '/founders/leandro_diaz.jpeg',
@@ -29,17 +41,7 @@ export const TEAM_MEMBERS = {
         profession: 'Desarrollador Full Stack',
         description:
             'Hola soy Leandro Diaz, estoy enfocado en aumentar mis conocimientos en el mundo de la programación y aprender nuevas tecnologías, con el interés en el desarrollo web. ',
-    },
-    Joaquin: {
-        fullname: 'Joaquin Dominguez',
-        logo: '/founders/joaquin_dominguez.jpeg',
-        email: 'juaquindominguez03@gmail.com',
-        gitHub: 'http://github.com/DOMINGUEZJOACOO',
-        linkedin: 'https://www.linkedin.com/in/joaquin-dominguez-0a3173356',
-        link: '/founders/joaquin_dominguez',
-        profession: 'Desarrollador Backend',
-        description:
-            '¡Hola! Mi nombre es Joaquín Domínguez, Me encuentro en formación constante en el mundo de la programación, con muchas ganas de seguir adquiriendo conocimientos de aquellos con experiencia.',
+        index: 3,
     },
     Lautaro: {
         fullName: 'Ferreyra Lautaro',
@@ -51,7 +53,7 @@ export const TEAM_MEMBERS = {
         profession: 'Desarrollador Full Stack',
         description:
             '¡Hola! Soy Lautaro Ferreyra, estudiante de la Tecnicatura Superior en Desarrollo de Software, con un marcado interés en el desarrollo backend y la creación de soluciones tecnológicas robustas.',
-        index: 3,
+        index: 4,
     },
     Briana: {
         fullname: 'Briana Montañez',
@@ -63,6 +65,7 @@ export const TEAM_MEMBERS = {
         profession: 'Desarrolladora Full Stack',
         description:
             '¡Hola! Soy Briana Montañez, Mi pasión por buscar soluciones viene desde chica, siguiendo las virtudes de mi padre por arreglar artefactos.',
+        index: 5,
     },
     Sofia: {
         fullName: 'Rubio Sofia',
@@ -74,18 +77,6 @@ export const TEAM_MEMBERS = {
         profession: 'Desarrolladora Full Stack',
         description:
             'Hola, soy Sofia Rubio, estudiante de Desarrollo de Software. Soy una persona comprometida y responsable, me gusta el desarrollo frontend y estoy a disposición para abordar desafíos en el backend.',
-        index: 4,
-    },
-    Julieta: {
-        fullName: 'Sotelo Julieta',
-        logo: '/founders/julieta_sotelo.jpeg',
-        email: 'soteloanzorena@gmail.com',
-        gitHub: 'https://github.com/JuuSotelo',
-        linkedin: 'https://www.linkedin.com/in/julieta-sotelo-54181a347',
-        link: '/founders/julieta_sotelo',
-        profession: 'Diseño, Análisis, Fullstack',
-        description:
-            '¡Hola! Soy Julieta Sotelo, estudiante de la Tecnicatura Superior en Desarrollo de software explorando el camino de la programación y diseño web.',
-        index: 5,
+        index: 6,
     },
 };
